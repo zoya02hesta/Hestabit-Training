50920ca55c1d2fb993472321720e821e17d7a685
Zoya Fatima <zoyaf.hestabit@gmail.com>
2026-02-18 19:39:44 +0530

    updating week3 as per day3

--- Week-3/Day-1/week-3-dashboard/app/layout.tsx
+++ Week-3/Day-1/week-3-dashboard/app/layout.tsx
@@ -1,28 +1,14 @@
-import "./globals.css"
-
-
-
-import Navbar from "@/components/ui/Navbar";
-import Sidebar from "@/components/ui/Sidebar";
 import "./globals.css";
 
-export default function RootLayout({ 
-    children,
+export default function RootLayout({
+  children,
 }: {
-  children: React.ReactNode
-
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className="bg-gray-100">
-        <Sidebar />
-        
-        <div className="ml-64">
-          <Navbar />
-          <main className="p-8">
-            {children}
-          </main>
-        </div>
+        {children}
       </body>
     </html>
   );
